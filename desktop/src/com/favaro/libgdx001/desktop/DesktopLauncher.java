5d04d1737392643be81197b206bb699e08b4c431
hygsani <blek.wind@gmail.com>
2017-03-29 14:00:37 +0700

    first commit

---
+++ desktop/src/com/favaro/libgdx001/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.favaro.libgdx001.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.favaro.libgdx001.LibGDX001;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDX001(), config);
+	}
+}
