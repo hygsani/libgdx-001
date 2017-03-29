538c42c5e1a39dedfe59a8c6da13cc1070fa5669
hygsani <blek.wind@gmail.com>
2017-03-29 14:13:59 +0700

    render bucket on the screen

--- core/src/com/favaro/libgdx001/LibGDX001.java
+++ core/src/com/favaro/libgdx001/LibGDX001.java
@@ -5,22 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LibGDX001 extends ApplicationAdapter {
 
 	private Sound dropSound;
 	private Music rainMusic;
+	private Texture bucketTexture;
+	private Texture dropletTexture;
 
 	private SpriteBatch batch;
+	private OrthographicCamera c;
+
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 480;
 
 	@Override
 	public void create () {
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		bucketTexture = new Texture(Gdx.files.internal("bucket.png"));
+		dropletTexture = new Texture(Gdx.files.internal("droplet.png"));
 
 		rainMusic.setLooping(true);
 		rainMusic.play();
+
+		c = new OrthographicCamera();
+		c.setToOrtho(false, WIDTH, HEIGHT);
 
 		batch = new SpriteBatch();
 	}
@@ -31,7 +44,7 @@ public class LibGDX001 extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
-
+		batch.draw(bucketTexture, WIDTH / 2, HEIGHT / 2);
 		batch.end();
 	}
 
