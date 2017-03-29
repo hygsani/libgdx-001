8d55d0270e9bb058d221558c5f02ca365f865444
hygsani <blek.wind@gmail.com>
2017-03-29 14:08:43 +0700

    play rain music in the background

--- core/src/com/favaro/libgdx001/LibGDX001.java
+++ core/src/com/favaro/libgdx001/LibGDX001.java
@@ -2,32 +2,41 @@ package com.favaro.libgdx001;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LibGDX001 extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private Sound dropSound;
+	private Music rainMusic;
+
+	private SpriteBatch batch;
+
 	@Override
 	public void create () {
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
