36b66f2dc8c339e0580f2c319938d2accb861a93
hygsani <blek.wind@gmail.com>
2017-03-29 14:26:23 +0700

    set desktop screen width and height

--- desktop/src/com/favaro/libgdx001/desktop/DesktopLauncher.java
+++ desktop/src/com/favaro/libgdx001/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.favaro.libgdx001.LibGDX001;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Simple 2D Game";
+		config.width = 800;
+		config.height = 480;
+
 		new LwjglApplication(new LibGDX001(), config);
 	}
 }
